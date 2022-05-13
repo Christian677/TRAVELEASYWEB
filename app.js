a1b960ffeb487e0cfe125664091aef188fa1c58b
MarcoLepDev <marcolepiane@gmail.com>
2022-05-13 18:29:09 +0200

    api call in jquery to get the object
    api call in jquery

--- app.js
+++ app.js
@@ -1,42 +1,19 @@
-let getDataButton = document.querySelector('#button');
+$('#button').on('click', getData);
 
 
-getDataButton.onclick = async function() {
 
-
-    const api_key = '2243bb736af6dfdad57ae3b1d912e4f69ec396c6';
-
-    let city = document.querySelector('#city').value;
-
-        //API CALL  with axios                   
-            axios.get(`https://api.waqi.info/feed/${city}/?token=${api_key}`)
-                        
-            .then(
-                
-            async response => {
-                
-            let data = await response.data
-
-        //if the request can't be processed (city is not found)
-
-            if (response.status !== 200) {
-            
-                throw new Error(response.status);
-            
-                } else {
-            
-
-        //if the call works and the request is being 
-            console.log(data);
-
-            //collecting data from API
-            }})
-                
-
-                //In case of errors, shows 
-                .catch (error =>
-                    alert(error));
-                
-                    
-
-            };
+function getData() {
+    var api_key="API_KEY";
+    let city = $('input').val();
+    $.ajax({
+        type: 'GET',
+        url: `https://api.waqi.info/feed/${city}/?token=${api_key}`,
+        datatype: 'JSON',
+        success: function (data){
+            console.log('success', data);
+        },
+        error: function(data){
+            console.log('error', data);
+        }
+    })
+}
