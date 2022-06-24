533ac360e3fa847461165716064f391b59599251
Firi0n <88585181+Firi0n@users.noreply.github.com>
2022-06-24 17:21:37 +0200

    prova2

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,6 +5,13 @@ namespace TRAVELEASYWEB.Pages;
 
 public class IndexModel : PageModel
 {
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
     public void OnGet()
     {
 
