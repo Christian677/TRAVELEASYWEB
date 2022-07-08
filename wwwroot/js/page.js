87326ca7adccc8625fa66ba74ea5dbbd17206e42
MarcoLepDev <marcolepiane@gmail.com>
2022-07-08 23:34:54 +0200

    solving bugs pt 2 + commenti ripristinati

--- wwwroot/js/page.js
+++ wwwroot/js/page.js
@@ -1,9 +1,87 @@
-﻿let btn = document.querySelector('#button');
+﻿/* 
+let searchButton = document.querySelector('#button').addEventListener('click', getData);
+let input = document.querySelector('input');
+
+
+function getData() {
+    const api_key = "";
+
+
+
+    var e = $.Event("keypress", { keyCode: 13 });
+    document.querySelector('#city').trigger(e);
+
+    //If input field is empty, message shows up
+
+    if (document.querySelector('input').value == '') {
+
+        document.querySelector('p').html('Please type a city name');
+
+
+        //if input field is NOT empty, api call goes on
+    } else {
+        document.querySelector('p').css('visibility', 'hidden');
+
+        $.ajax({
+            type: 'GET',
+            url: '',
+            datatype: 'JSON'
+        })
+
+            .done(function (data) {
+
+                // if city exists...
+
+                if (data.status == 'ok') {
+                    document.querySelector('p').css('visibility', 'hidden');
+                    console.log('corretto', data);
+
+                    //if city doesn't exist, show message to user
+                } else if (data.status == 'error') {
+                    document.querySelector('p').css('visibility', 'visible');
+                    document.querySelector('p').html('Please insert a valid city name');
+                }
+            })
+
+            // if server doesn't respond...
+            .fail(function (data) {
+                console.log('fail', data);
+
+            })
+
+    }
+}
+
+//if users presses enter in the input field
+
+document.querySelector('input').addEventListener('keypress', function (e) {
+    if (e.which == 13 && input.value == "") {
+        alert('stringa vuota');
+    } else if (e.which == 13 && input.value != "") {
+        getData();
+    }
+});
+
+
+// search bar opens up on click
+
+document.querySelector('.open_close_search').click(function () {
+    if (document.querySelector('.input-container').css('visibility') == 'hidden')
+        document.querySelector('.input-container').css('visibility', 'visible');
+    else
+        document.querySelector('.input-container').css('visibility', 'hidden');
+});
+ */
+let inputField = document.querySelector('input');
+let btn = document.querySelector('#button');
+let errorOne = document.querySelector('#message');
+let errorContainer = document.querySelector('.message-container');
+
 
 btn.onclick = async function getData() {
     let city = document.querySelector('#city').value;
 
-    const api_key = '';
+    const api_key = '2243bb736af6dfdad57ae3b1d912e4f69ec396c6';
 
 
     //API CALL  with axios                   
@@ -20,29 +98,48 @@ btn.onclick = async function getData() {
                 if (response.status !== 200) {
 
                     throw new Error(response.status);
+                    console.log(response);
+                    console.log('città non trovata', 'errore 200');
 
                 } else {
 
+                    //if call works but city doesn't exist - user has typed a wrong city
+                    if (data.status == 'error') {
+                        errorOne.style.visibility = 'visible';
+                        errorOne.style.color = "#000000";
+                        errorOne.textContent = 'City name is invalid or not in the database';
+                    }
+                    //if call works and city exists
+                    else if (data.status == 'ok') {
+                        errorOne.style.visibility = 'hidden';
+                        console.log('corretto:', data);
+                    }
 
-                    //if the call works and the request is being 
-                    console.log(data);
+                    
+
                 }
+
             })
 
 
         //In case of errors, shows 
         .catch(error =>
-            alert('Please type a valid city name'));
+            console.log(error.message));
+
 
 
 
 };
 
+
+
+
+
+
 // search bar opens up or closes on click
-let openbar = document.querySelector('.open_close_search');
+let openSearchBar = document.querySelector('.open_close_search');
 
-
-openbar.addEventListener('click', function () {
+openSearchBar.addEventListener('click', function () {
 
     
     if (document.querySelector('.input-container').style.visibility == 'hidden') {
@@ -53,12 +150,13 @@ openbar.addEventListener('click', function () {
 })
 
 
-//if users presses enter in the input field
-inputField.addEventListener('keydown', function (e) {
 
-    if (e.keyCode === 13 && inputField.value == '') {
-        alert('key was pressed');
-    } else if (e.keyCode === 13 && !inputField.value == '') {
-        btn.onclick();
-    }
-});
+//inputField.addEventListener('keydown', function (e) {
+
+  //  if (e.keyCode === 13 && inputField.value == '') {
+  //      errorOne.style.color = "#000000";
+  //      errorOne.textContent = 'City name is missing!';
+  //  } else if (e.keyCode === 13 && !inputField.value == '') {
+  //      btn.onclick();
+  //  }
+//});
